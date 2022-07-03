9317fdac7872df2cd5c39b3f8f50b985a888025b
Ghabryel <ghabryelhenrique@hotmail.com>
2022-07-03 09:35:28 -0300

    Ajuste loop multiplas licitações simultâneos

--- main.py
+++ main.py
@@ -11,25 +11,30 @@ def getObject():
     options.add_argument("headless")
     browser = webdriver.Chrome(options=options)
     browser.get('https://bnccompras.com/Process/ProcessSearchActivity')
-    time.sleep(5)
+    time.sleep(2)
+
+    atividade = browser.find_element(By.XPATH, '//*[@id="fkActivity"]')
+    select_atividade = Select(atividade)
+    select_atividade.select_by_value('128')
 
     for x in range(1, 28):
         try:
-            atividade = browser.find_element(By.XPATH, '//*[@id="fkActivity"]')
-            select_atividade = Select(atividade)
-            select_atividade.select_by_value('128')
 
             estado = browser.find_element(By.CSS_SELECTOR, '#fkState')
             select_estado = Select(estado)
             select_estado.select_by_value('{}'.format(x))
 
             browser.find_element(By.XPATH, '//*[@id="btnAuctionSearch"]').click()
-            time.sleep(5)
+
+            time.sleep(2)
 
             objeto = browser.find_element(By.XPATH, '//*[@id="tableProcessDataBody"]/tr/td[4]').text
             print(objeto)
 
+            browser.find_element(By.XPATH, '//*[@id="tableProcessDataBody"]/tr/td[1]/a').click()
+
         except:
             print('Nada novo')
+
 
 getObject()
